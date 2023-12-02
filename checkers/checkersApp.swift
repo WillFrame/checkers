ad251799b844189d74e35a0de6d7bcc74b04df30
Matvey <mozhevm@gmail.com>
2023-12-02 18:37:02 +0600

    Initial Commit

---
+++ checkers/checkersApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkersApp.swift
+//  checkers
+//
+//  Created by Матвей Можевикин on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct checkersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
