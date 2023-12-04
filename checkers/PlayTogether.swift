f4f125d65c98127301fa0d47182bb42e2b62d634
Matvey <mozhevm@gmail.com>
2023-12-05 01:57:03 +0600

    fix

---
+++ checkers/PlayTogether.swift
@@ -0,0 +1,8 @@
+//
+//  PlayTogether.swift
+//  checkers
+//
+//  Created by Матвей Можевикин on 05.12.2023.
+//
+
+import Foundation
---
+++ checkers/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  checkers
+//
+//  Created by Матвей Можевикин on 05.12.2023.
+//
+
+import Foundation
