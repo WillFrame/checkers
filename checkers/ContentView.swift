e05f545ef9e19082678db5e9ac2480f45cec686d
Matvey <mozhevm@gmail.com>
2023-12-05 00:38:27 +0600

    fix

--- checkers/ContentView.swift
+++ checkers/ContentView.swift
@@ -7,32 +7,62 @@
 
 import SwiftUI
 
+struct PlayTogether: View {
+	var body: some View {
+		VStack {
+			Text("Играть вдвоем")
+			Text("ТЕСТ")
+		}
+	}
+}
+
+// третий экран (открывающийся по ссылке с первого)
+struct Settings: View {
+	var body: some View {
+		VStack {
+			Text("Настройки")
+			Text("ТЕСТ")
+		}
+	}
+}
+
 struct ContentView: View {
     @State var tapCount = 0
+	@State private var tagSelection: String? = nil
     
     var body: some View {
         VStack {
-            Button(
-                action: {
-                    print("Играть вдвоем")
-                }
-            ) {
-                Text("Играть вдвоем")
-            }
-			Button(
-				action: {
-					print("Тестовая кнопка")
+			NavigationView {
+				VStack {
+					NavigationLink(
+						destination: PlayTogether(),tag: "Играть вдвоем", selection: $tagSelection
+					) { EmptyView() }
+					NavigationLink(destination: Settings(), tag: "Настройки", selection: $tagSelection) { EmptyView() }
+					
+					Button(
+						
+						action: {tagSelection = "Играть вдвоем"}
+					) {
+						Text("Играть вдвоем")
+							.padding([.horizontal], 30)
+							.padding([.vertical], 10)
+							.background(.brown)
+							.cornerRadius(20)
+							.foregroundStyle(.black)
+					}
+					Button(
+						action: {tagSelection = "Настройки"}
+					) {
+						Text("Настройки")
+							.padding([.horizontal], 30)
+							.padding([.vertical], 10)
+							.background(.brown)
+							.cornerRadius(20)
+							.foregroundStyle(.black)
+					}
 				}
-			) {
-				Text("Тестовая кнопка")
+				.navigationTitle("Главное меню")
 			}
-            Button(
-                action: {
-                    print("Настройки")
-                }
-            ) {
-                Text("Настройки")
-            }
         }
     }
 }
